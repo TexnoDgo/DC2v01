592a06aeecdd67fc34e9a46fd246cafce305923f
TexnoDgo <texnodgo@gmail.com>
2021-01-05 18:20:31 +0200

    some change

--- DC2v01Web/WorkApp/send_file_file.py
+++ DC2v01Web/WorkApp/send_file_file.py
@@ -28,9 +28,9 @@ def send_comp_draw_png(component_name):
     try:
         print("Начат процесс получение png чертежа компонента")
         component = Component.objects.get(title=component_name)
-        draw = component.draw_pdf
-        response = HttpResponse(FileWrapper(draw), content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="%s"' % '111.pdf'
+        draw = component.draw_png
+        response = HttpResponse(FileWrapper(draw), content_type='image/png')
+        response['Content-Disposition'] = 'attachment; filename="%s"' % '111.png'
         print("Процесс завершен!")
         return response
     except Exception:
